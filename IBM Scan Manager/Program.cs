9746eff3731663fd5fe69ec5454f5326201a0a2e
Rajitha Dulanjith <rajitha.dulanjith@infor.com>
2021-08-04 11:43:49 +0530

    View findings page fixed. Sorting remains

--- IBM Scan Manager/Program.cs
+++ IBM Scan Manager/Program.cs
@@ -18,7 +18,7 @@ namespace IBM_Scan_Manager
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmViewFindings());
+            Application.Run(new frmProject());
         }
     }
 }
